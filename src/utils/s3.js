12cc30c7f5c2a1f4aaba0212023f5faae5197b26
JuanMorte <alexanderpratam@gmail.com>
2025-05-31 14:46:13 +0800

    new update

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../../.env' });
 
 export const getS3ImageUrl = (imageKey) => {
-    return `https://${import.meta.env.AWS_BUCKET_NAME}.s3.${import.meta.env.AWS_REGION}.amazonaws.com/${imageKey}`;
+    return `https://${import.meta.env.AWS_BUCKET_NAME}.s3.${import.meta.env.DATALENS_AWS_REGION}.amazonaws.com/${imageKey}`;
 }
